18e46ff5a83d8de4498a749a757bb6187e749a94
csavatia <savatiacedric@gmail.com>
2022-01-31 22:15:55 +0300

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const {
+    UnauthenticatedError
+} = require('../errors')
+const jwt = require('jsonwebtoken')
+
+const authenticationMiddleware = async (req, res, next) => {
+    // consolele.log(req.headers.authorization)
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new UnauthenticatedError('No token provided')
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        const {
+            id,
+            username
+        } = decode
+        req.user = {
+            id,
+            username
+        }
+        next()
+    } catch (error) {
+        throw new UnauthenticatedError('Not authorized to access this route')
+    }
+
+}
+
+module.exports = authenticationMiddleware
